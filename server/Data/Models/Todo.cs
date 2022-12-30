0af7d363b79f7b4f905fc241f628b3bbf2c008a0
Sangjin Kim <rla2892@naver.com>
2022-12-30 20:01:40 +0900

    create todo class

---
+++ server/Data/Models/Todo.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace sangjin
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
+        public DateTime? DoneTime { get; set; }
+        public bool IsDone
+        {
+            get => this.DoneTime.HasValue;
+        }
+    }
+}
