17023e275450e4609944fa06442b6e6f23481704
Sangjin Kim <rla2892@naver.com>
2022-12-31 15:21:03 +0900

    Migrate database

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ Todo List Application
 - ```bash
   dotnet new webapi -f netcoreapp3.1
   ```
+
+## Update
+
+### Update (DB)
+- ```bash
+  # Visual Studio 의 Package Management Console 에서 수행
+  
+  # Migration Code 생성
+  Add-Migration InitTodoListDb
+
+  # DB에 반영
+  Update-Database
+  ```
---
+++ server/Migrations/20221231061341_InitTodoListDb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoList.Migrations
+{
+    public partial class InitTodoListDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Content = table.Column<string>(nullable: true),
+                    CreatedTime = table.Column<DateTime>(nullable: false),
+                    UpdatedTime = table.Column<DateTime>(nullable: false),
+                    DoneTime = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todo");
+        }
+    }
+}
