eda78eba1250b24de52c54afcda81b7fee072805
Sangjin Kim <rla2892@naver.com>
2022-12-31 15:35:05 +0900

    create service class

---
+++ server/Service/TodoService.cs
@@ -0,0 +1,13 @@
+﻿using TodoList.Data.Context;
+
+namespace TodoList.Service
+{
+    public class TodoService
+    {
+        private AppDbContext _context;
+        public TodoService(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
