e09ef3ef3caad0759c3d210d87446a9ea19cbc17
Sangjin Kim <rla2892@naver.com>
2023-01-07 18:42:50 +0900

    http 2.0 관련 comments 추가

--- server/Program.cs
+++ server/Program.cs
@@ -21,6 +21,10 @@ namespace TodoList
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    // NOTE : .net core 3.0 이상에서는 http 2.0 이 default
+                    // NOTE : http2 사용 시, request size 감소(크롬 개발자 툴에서 확인)
+                    // NOTE : http2 사용 시, protocol 이 h2 임 (http/1.1 이 아님)
+                    // NOTE : (IIS) kestrel 의 경우 아래 옵션으로 http2.0 을 enable/disable 할 수 있으나, IIS 는 이를 설정할 수 없음. client/server 가 둘 다 http 2.0 사용 가능하면 자동으로 2.0 사용 그외에는 1.1 사용
                     webBuilder.ConfigureKestrel((options) =>
                     {
                         // trying to use Http1AndHttp2 causes http2 connections to fail with invalid protocol error
