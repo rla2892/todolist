48aaf6d102a18c79b44c2cf112a1f089d94f09b9
Sangjin Kim <rla2892@naver.com>
2023-01-07 17:23:42 +0900

    kestrel 서버에서 http2 사용

--- server/Program.cs
+++ server/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -20,6 +21,12 @@ namespace TodoList
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureKestrel((options) =>
+                    {
+                        // trying to use Http1AndHttp2 causes http2 connections to fail with invalid protocol error
+                        // according to Microsoft dual http version mode not supported in unencrypted scenario: https://learn.microsoft.com/en-us/aspnet/core/grpc/troubleshoot?view=aspnetcore-3.0
+                        options.ConfigureEndpointDefaults(lo => lo.Protocols = HttpProtocols.Http2);
+                    });
                     webBuilder.UseStartup<Startup>();
                 });
     }
